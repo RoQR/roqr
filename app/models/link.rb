2d6ba125e751db86c03fc57f1afe48dcb4ae2a4c
Sebastian Rollen <seb@rollen.io>
2022-06-09 19:19:02 -0400

    cascade delete and add date column

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,6 @@ class Link < ApplicationRecord
   validates :name, presence: true
   validates :url, presence: true, url: true
 
-  has_many :events
+  has_many :events, dependent: :delete_all
 end
 
