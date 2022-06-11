1014ff3d6c01153dc5b444f25551ef3eba921740
Sebastian Rollen <seb@rollen.io>
2022-06-11 14:46:34 -0400

    fix sign outs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :success, :warn
+
+  protected
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
