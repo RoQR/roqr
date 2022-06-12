28b5e262a7969dcc9fd2517558b9ee96e03689cb
Sebastian Rollen <seb@rollen.io>
2022-06-12 17:04:34 -0400

    add mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "roqr_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'roqr.app',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication:       'plain'
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
