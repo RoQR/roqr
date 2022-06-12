cf1320685d9db32dc895cd9d825ddf2db15201b6
Sebastian Rollen <seb@rollen.io>
2022-06-12 16:57:46 -0400

    add in confirmable devise module

--- app/abilities/ability.rb
+++ app/abilities/ability.rb
@@ -4,8 +4,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, Link, user: user
     can :read, Event, link: {user: user}
+    can :read, Link, user: user
+    if user.confirmed?
+      can :manage, Link, user: user
+    end
     can :manage, user
   end
 end
