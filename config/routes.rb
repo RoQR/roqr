9e3ad18dd1341f76e057ff1db68b4c1b81e56d51
Sebastian Rollen <seb@rollen.io>
2022-06-12 14:32:31 -0400

    add unauthenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'user/sessions', passwords: 'user/passwords', registrations: 'user/registrations' }
 
-  root 'links#index'
   resources :links do
     get :scan, on: :member
   end
   resources :users
   resources :events, only: :index
+
+  authenticated :user do
+    root to: 'links#index', as: :authenticated_root
+  end
+
+  devise_scope :user do
+    root to: 'user/sessions#new'
+  end
 end
