5bb03b570829a7247b7f3e77d25f7081963e112a
Sebastian Rollen <seb@rollen.io>
2022-06-12 20:26:05 -0400

    fix layout for 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def page_not_found
     respond_to do |format|
-      format.html { render template: 'errors/not_found_error', layout: 'layouts/application', status: 404 }
+      format.html { render template: 'errors/not_found_error', layout: 'layouts/empty', status: 404 }
       format.all  { render nothing: true, status: 404 }
     end
   end
