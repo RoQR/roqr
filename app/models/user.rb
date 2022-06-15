f1b139c1fbd1d8ddf4f68e0d91cd1c441d4e02b4
Sebastian Rollen <seb@rollen.io>
2022-06-15 18:19:12 -0400

    savepoint

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,11 @@ class User < ApplicationRecord
     gravatar_id = Digest::MD5::hexdigest(email.downcase)
     "http://secure.gravatar.com/avatar/#{gravatar_id}?d=identicon"
   end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
