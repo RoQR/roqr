d747afc5a155d6145e43ea6e21bb1a74280c16d8
Sebastian Rollen <seb@rollen.io>
2022-06-15 19:03:57 -0400

    remove some fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,6 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
