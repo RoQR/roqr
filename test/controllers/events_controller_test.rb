479dcb70d11bc71d81664e5750c7d673dbf178a1
Sebastian Rollen <seb@rollen.io>
2022-06-28 07:51:06 -0400

    fix tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,14 +1,15 @@
-require "test_helper"
+require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
     @user = create(:user)
+    @user.confirm
     sign_in @user
   end
 
-  test "should get index when logged in" do
+  test 'should get index when logged in' do
     get events_url
     assert_response :success
   end
