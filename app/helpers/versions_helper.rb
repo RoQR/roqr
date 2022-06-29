49b7c96adbf696a48a159663b5729702edfbc262
Sebastian Rollen <seb@rollen.io>
2022-06-28 20:10:12 -0400

    safe operator

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,6 +1,6 @@
 module VersionsHelper
   def humanize_version_change(changeset)
-    changeset.reject do |k|
+    changeset&.reject do |k|
       k == 'updated_at'
     end.map { |k, vs| k == 'password_digest' ? 'changed password' : "#{k} from #{vs[0]} to #{vs[1]}" }.to_sentence
   end
