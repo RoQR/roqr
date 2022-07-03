3ef16f1967b6360c9cbe97a837293b9ee82861ff
Sebastian Rollen <seb@rollen.io>
2022-07-03 10:56:58 -0400

    only use sentry in production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = 'https://fbb1f70d628842af87504b368db90218@o1295260.ingest.sentry.io/6520504'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.enabled_environments = %w[production]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
