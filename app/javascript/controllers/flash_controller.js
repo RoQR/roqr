cba01ef0c277b69224830ece901aae41fcc312b5
Sebastian Rollen <seb@rollen.io>
2022-07-05 21:16:57 -0400

    refactor some things in flash controller

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -6,28 +6,25 @@ export default class extends Controller {
   connect() {
     const timeoutSeconds = parseInt(this.data.get("timeout"));
 
-    if (!this.isPreview) {
-      // Display with transition
+    // Display with transition
+    setTimeout(() => {
+      this.element.classList.remove('hidden');
+      this.element.classList.add('transform', 'ease-out', 'duration-300', 'transition', 'translate-y-2', 'opacity-0', 'sm:translate-y-0', 'sm:translate-x-2');
+
+      // Trigger transition
       setTimeout(() => {
-        this.element.classList.remove('hidden');
-        this.element.classList.add('transform', 'ease-out', 'duration-300', 'transition', 'translate-y-2', 'opacity-0', 'sm:translate-y-0', 'sm:translate-x-2');
+        this.element.classList.add('translate-y-0', 'opacity-100', 'sm:translate-x-0');
+      }, 100);
 
-        // Trigger transition
-        setTimeout(() => {
-          this.element.classList.add('translate-y-0', 'opacity-100', 'sm:translate-x-0');
-        }, 100);
+      if (this.hasCountdownTarget) {
+        this.countdownTarget.style.animation = `flash-countdown linear ${timeoutSeconds}s`;
+      }
+    }, 500);
 
-        if (this.hasCountdownTarget) {
-          this.countdownTarget.style.animation = `flash-countdown linear ${timeoutSeconds}s`;
-        }
-
-      }, 500);
-
-      // Auto-hide
-      this.timeoutId = setTimeout(() => {
-        this.close();
-      }, timeoutSeconds * 1000 + 500);
-    }
+    // Auto-hide
+    this.timeoutId = setTimeout(() => {
+      this.close();
+    }, timeoutSeconds * 1000 + 500);
   }
 
   run(e) {
@@ -84,10 +81,6 @@ export default class extends Controller {
     setTimeout(() => {
       this.element.remove();
     }, 300);
-  }
-
-  get isPreview() {
-    return document.documentElement.hasAttribute('data-turbolinks-preview')
   }
 
   get csrfToken() {
