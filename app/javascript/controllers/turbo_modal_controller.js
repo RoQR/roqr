24ce2d1fb19edb4c970ff2a9ab73a38ec3716b9d
Sebastian Rollen <seb@rollen.io>
2022-07-06 20:22:57 -0400

    close modal with escape

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -4,26 +4,32 @@ import {enter, leave} from "el-transition"
 export default class extends Controller {
   static targets = ["backdrop", "body"]
   connect() {
-    document.addEventListener('turbo:submit-end', this.submitEnd)
+    document.addEventListener('turbo:submit-end', this.handleSubmit)
     enter(this.backdropTarget);
     enter(this.bodyTarget);
   }
 
   disconnect() {
-    document.removeEventListener('turbo:submit-end', this.submitEnd)
+    document.removeEventListener('turbo:submit-end', this.handleSubmit)
   }
 
   hideModal() {
     event.preventDefault();
     leave(this.bodyTarget);
     leave(this.backdropTarget).then(() => {
-        this.element.parentElement.removeAttribute("src");
-        this.element.remove();
+      this.element.parentElement.removeAttribute("src");
+      this.element.remove();
     })
   }
 
-  submitEnd = (e) => {
+  handleSubmit = (e) => {
     if (e.detail.success) {
+      this.hideModal()
+    }
+  }
+
+  handleKeyup(e) {
+    if (e.code == "Escape") {
       this.hideModal()
     }
   }
