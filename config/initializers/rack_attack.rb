a5c85621105c9b13bf7c0a9982213a6d46042ed1
Sebastian Rollen <seb@rollen.io>
2022-07-19 19:02:04 -0400

    better throttling

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,15 @@
-class Rack::Attack
-  throttle('req/ip', limit: 5, period: 1.seconds) do |req|
-    req.ip
-  end
+Rack::Attack.throttle('req/ip', limit: 300, period: 5.minutes) do |req|
+  req.ip
+end
+Rack::Attack.throttled_responder = lambda do |request|
+  match_data = request.env['rack.attack.match_data']
+  now = match_data[:epoch_time]
+
+  headers = {
+    'RateLimit-Limit' => match_data[:limit].to_s,
+    'RateLimit-Remaining' => '0',
+    'RateLimit-Reset' => (now + (match_data[:period] - now % match_data[:period])).to_s
+  }
+
+  [429, headers, ["Throttled\n"]]
 end
