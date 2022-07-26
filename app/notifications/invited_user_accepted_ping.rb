3a8263d6077e6034175a14728c5c741b6e8acbd0
Sebastian Rollen <seb@rollen.io>
2022-07-26 17:02:33 -0400

    link to users page in notification

--- app/notifications/invited_user_accepted_ping.rb
+++ app/notifications/invited_user_accepted_ping.rb
@@ -1,27 +1,11 @@
-# To deliver this notification:
-#
-# InvitedUserAcceptedPing.with(post: @post).deliver_later(current_user)
-# InvitedUserAcceptedPing.with(post: @post).deliver(current_user)
-
 class InvitedUserAcceptedPing < Noticed::Base
-  # Add your delivery methods
-  #
+  include ActionView::Helpers::UrlHelper
   deliver_by :database
   # deliver_by :email, mailer: "UserMailer"
-  # deliver_by :slack
-  # deliver_by :custom, class: "MyDeliveryMethod"
 
-  # Add required params
-  #
   param :user
 
-  # Define helper methods to make rendering easier.
-  #
   def message
-    "#{params[:user].name} accepted your invitation"
+    link_to "#{params[:user].name} accepted your invitation", user_path(params[:user].id)
   end
-  #
-  # def url
-  #   post_path(params[:post])
-  # end
 end
