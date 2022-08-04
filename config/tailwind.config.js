7edb1d4f4f556684c95be00347765fba68ae3394
Sebastian Rollen <seb@rollen.io>
2022-08-03 20:46:18 -0400

    use neutral grays

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-      gray: colors.slate,
+      gray: colors.neutral,
       // gray: {
       //   50: '#C4CCD4',
       //   100: '#ACB8C3',
