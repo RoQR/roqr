8c46f4305078da0d3b4b12bed96f1e45c4a5ea93
Sebastian Rollen <seb@rollen.io>
2022-09-09 16:22:37 -0500

    fix empty state on styles

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.dsn = 'https://fbb1f70d628842af87504b368db90218@o1295260.ingest.sentry.io/6520504'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.enabled_environments = %w[production]
 
   # Set traces_sample_rate to 1.0 to capture 100%
@@ -8,7 +8,7 @@ Sentry.init do |config|
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |context|
+  config.traces_sampler = lambda do |_context|
     true
   end
 end
