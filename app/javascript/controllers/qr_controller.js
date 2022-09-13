c4a26a6ddd5b7a5410a891409ab0af0e609f8df4
Sebastian Rollen <seb@rollen.io>
2022-09-13 18:56:28 -0400

    add margin to image

--- app/javascript/controllers/qr_controller.js
+++ app/javascript/controllers/qr_controller.js
@@ -56,6 +56,9 @@ export default class extends Controller {
       },
       backgroundOptions: {
         color: this.backgroundColorTarget.value
+      },
+      imageOptions: {
+        margin: 10
       }
     }
   }
