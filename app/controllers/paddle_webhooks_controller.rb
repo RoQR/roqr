57275ff9c471f36a41fa76a46cadad5f11142a4a
Sebastian Rollen <seb@rollen.io>
2022-09-14 18:42:36 -0400

    add webhook handler for subscription updates

--- app/controllers/paddle_webhooks_controller.rb
+++ app/controllers/paddle_webhooks_controller.rb
@@ -7,6 +7,7 @@ class PaddleWebhooksController < ApplicationController
     when 'subscription_created'
       subscription_created
     when 'subscription_updated'
+      subscription_updated
     when 'subscription_cancelled'
     when 'subscription_payment_succeeded'
     when 'subscription_payment_failed'
@@ -19,6 +20,17 @@ class PaddleWebhooksController < ApplicationController
     organization.build_subscription(paddle_subscription_params)
     respond_to do |format|
       if organization.save
+        format.html { head 200 }
+      else
+        format.html { head 500 }
+      end
+    end
+  end
+
+  def subscription_updated
+    organization = Organization.find(params[:passthrough])
+    respond_to do |format|
+      if organization.subscription.update(paddle_subscription_params)
         format.html { head 200 }
       else
         format.html { head 500 }
