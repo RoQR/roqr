f122eb1791ac8284a5c4e1c46c1ca0970750ab6c
Sebastian Rollen <seb@rollen.io>
2022-09-15 17:43:51 -0400

    cancel paddle subscription on subscription deletion

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,9 @@
 class Subscription < ApplicationRecord
+  before_destroy :cancel_paddle_subscription
   belongs_to :organization
   alias_attribute :subscription_id, :paddle_subscription_id
+
+  def cancel_paddle_subscription
+    PaddlePay::Subscription::User.cancel(subscription_id) unless status == 'deleted'
+  end
 end
