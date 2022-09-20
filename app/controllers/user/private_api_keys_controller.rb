cd5eec9081c21ba2df2ba8581ced7ee85b1cad74
Sebastian Rollen <seb@rollen.io>
2022-09-20 19:56:00 -0400

    revert class to module change

--- app/controllers/user/private_api_keys_controller.rb
+++ app/controllers/user/private_api_keys_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-module User
-  class PrivateApiKeysController < ApplicationController
-    before_action :authenticate_user!
-    def update
-      if current_user.update(private_api_key: SecureRandom.hex)
-        redirect_to settings_developer_path, success: 'API key updated'
-      else
-        redirect_to settings_developer_path,
-                    warn: "There was an error: #{current_user.errors.full_messages.to_sentence}"
-      end
+class User::PrivateApiKeysController < ApplicationController
+  before_action :authenticate_user!
+  def update
+    if current_user.update(private_api_key: SecureRandom.hex)
+      redirect_to settings_developer_path, success: 'API key updated'
+    else
+      redirect_to settings_developer_path,
+                  warn: "There was an error: #{current_user.errors.full_messages.to_sentence}"
     end
   end
 end
