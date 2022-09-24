d907730e12f86dc06ae7942882137d4b3349eafe
Sebastian Rollen <seb@rollen.io>
2022-09-24 16:18:12 -0400

    move paddle back to normal js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-Paddle.Environment.set('sandbox');
-Paddle.Setup({ vendor: 7904 });
 
--- app/javascript/controllers/paddle_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static values = { productId: Number, email: String, passthrough: String }
-
-  checkout() {
-    Paddle.Checkout.open({email: this.emailValue, product: this.productIdValue, passthrough: this.passthroughValue})
-  }
-}
