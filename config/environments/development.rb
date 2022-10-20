04b78d3df5afa887aef826b5adfe79b928a1c36a
Sebastian Rollen <seb@rollen.io>
2022-10-20 17:48:43 -0400

    clear hosts on development server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.hosts << 'sebastians-macbook-pro.sebrollen.github.beta.tailscale.net'
+  config.hosts.clear
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
