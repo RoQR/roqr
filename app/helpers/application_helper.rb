fb1aac3d33634f98e2bcad07046163e92ad1e755
Sebastian Rollen <seb@rollen.io>
2022-10-24 21:51:27 -0400

    use button component for most buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   end
 
   def render_component(component_path, collection: nil, **options, &block)
-    component_klass = "#{component_path.classify}Component".constantize
+    component_klass = "#{component_path.to_s.classify}Component".constantize
 
     if collection
       render component_klass.with_collection(collection, **options), &block
