f3d010979b12a70f716038f36b75a578409edbe5
Sebastian Rollen <seb@rollen.io>
2022-11-14 20:10:27 -0500

    setup to use fly

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_220038) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "ahoy_events", force: :cascade do |t|
     t.bigint "visit_id"
     t.bigint "user_id"
