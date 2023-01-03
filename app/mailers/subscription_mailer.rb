280f56e37a38ad5b5c34c254c22ca24fba81a782
Sebastian Rollen <seb@rollen.io>
2023-01-03 17:28:02 -0500

    don't send trial_ended email if user is unconfirmed

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,6 +1,8 @@
 class SubscriptionMailer < ApplicationMailer
   def trial_ended
     @user = params[:user]
+    return unless @user.confirmed?
+
     mail(to: @user.email, subject: "Your RoQR trial has ended")
   end
 end
