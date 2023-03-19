7e9c3636e16dd05b3804779475f6ac5e34cf1cde
Sebastian Rollen <seb@rollen.io>
2023-03-19 09:56:18 -0400

    fix sort order of subscription payments

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,19 +4,22 @@ class SettingsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_resources
 
-  def profile; end
+  def profile
+  end
 
-  def organization; end
+  def organization
+  end
 
   def subscription
     return unless @organization.subscribed?
 
-    @subscription_payments = @organization.subscription.subscription_payments.page params[:page]
+    @subscription_payments = @organization.subscription.subscription_payments.order(created_at: :desc).page params[:page]
     @last_payment_date = @subscription_payments.pluck(:event_time).max
     @num_scans = Scan.accessible_by(current_ability).where("created_at > ?", @last_payment_date).count
   end
 
-  def settings; end
+  def settings
+  end
 
   private
 
